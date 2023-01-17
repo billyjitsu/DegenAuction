e700f47bde4dfcc932422aa01a4164d7050c1c9c
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-01-16 16:49:43 -0800

    Initial commit

---
+++ contracts/Auction.sol
@@ -0,0 +1,96 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./NFTEscrow.sol";
+
+contract Auction is NFTEscrow, Ownable {
+
+    address public highestBidder;
+    
+    bool auctionStarted;
+    bool auctionEnded;
+
+    
+    uint256 public highestBid;
+    uint256 public auctionStartTime;
+    uint256 public auctionEndTime;
+    uint256 public valueLocked;
+
+    mapping(address => uint256) public  myAuctions;
+
+    mapping(address => uint256) public deposits;
+
+    struct NFTAuction { //fix this
+        address auctionCreator;
+        address nftContract;
+        uint256 auctionNumber;
+        uint256 depositId;
+        address finalBidder;
+        uint256 theHighestBid;
+        // bool claimed;
+        // mapping(address => bool) voters;
+    }
+
+    constructor () {
+    }
+
+    function startAuction(address _contractAddress, uint256 tokenId) public {
+        auctionStarted = true;
+        auctionStartTime = block.timestamp;
+        auctionEndTime = (auctionStartTime + 3 minutes) ;  //base in minutes, day = 1440 minutes, 1 week = 10080
+        registerAuction(_contractAddress, tokenId); 
+        //get the address of  who does this 
+    }
+
+    function bid(uint256 _bid) public payable {
+        require(auctionStarted == true, "Auction Hasn't started.");
+        require(_bid > highestBid, "Bid is not high enough.");
+        require(block.timestamp < auctionEndTime, "Auction has already ended.");
+        require(_bid <= msg.value, "Bid does not match deposited value.");
+        // Return previous highest bidder's deposit
+        if (highestBidder != address(0)) {
+            //payable(highestBidder).transfer(deposits[highestBidder]);
+            uint256 bonus = calculateBonus(_bid);
+           // uint256 payout = (bonus + deposits[highestBidder]);
+            (bool success, ) = payable(highestBidder).call{value: bonus + deposits[highestBidder]}("");
+            require(success, "payment not sent");
+        }
+
+        highestBidder = msg.sender;
+        highestBid = _bid;
+        deposits[msg.sender] = msg.value;
+    }
+
+    function withdrawAuctionFunds(INFT _token, uint256 _tokenId, uint256 _depositId) public  {
+        require(msg.sender == depositors[_depositId].nftOwner, "Not Auctioneer");
+        require(block.timestamp >= auctionEndTime, "Auction has not ended.");
+        withdrawToken(_token, _tokenId, _depositId, highestBidder);
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "Safe not sent");
+    }
+
+    // allow this contract to get permission to move from another project
+    // must check to make sure this is the owner of the token
+    // function setBiddingAllowed(address _contract, bool _value) external override onlyTokenOwner(_contract) {
+    //     collections[_contract].biddingAllowed = _value;
+    // }
+
+    function calculateBonus(uint256 _bid) public view returns(uint256) { //should be internal
+        //calculate the old bid and new bid to pay the bonus
+        uint256 difference = ((_bid - highestBid) * 5 / 100);
+        return difference;
+    }
+
+    function willFinishAt () public view returns(uint256) {
+        if(auctionStartTime == 0){
+            return 0;
+        } else {
+        uint256 time = auctionStartTime + 60 minutes;
+        return time;
+        }
+    }
+}
+
+
---
+++ contracts/NFTEscrow.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+interface INFT {
+    function setApprovalForAll(address operator, bool approved) external;
+    function balanceOf(address owner) external view returns (uint256);
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) external;
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+}
+
+contract NFTEscrow is IERC721Receiver {
+    using Counters for Counters.Counter;
+    Counters.Counter private _depositorIdCounter;
+    
+    // mapping(uint256 => address) public NFTOwner;
+    // mapping(uint256 => uint256) public tknIdOwner;
+
+     struct Depositor {
+        address nftOwner;
+        address nftContract;
+        uint256 tknIdOwner;
+        uint256 depositId;
+        // uint256 finalBid;
+         bool claimed;
+        // mapping(address => bool) voters;
+    }
+    
+    
+    uint256 public _tokenIdCounter = 1;
+    uint256 public tokensRecieved = 0;
+    INFT paperNft;
+
+    mapping(uint256 => Depositor) public  depositors;
+    //Depositor public depositor;
+   // constructor(address nftContract)  {
+    constructor()  {
+      //  _depositorIdCounter.increment();
+      //  paperNft = INFT(nftContract);
+    }
+
+    function registerAuction(address _contractAddress, uint256 tokenId) internal {
+        //Have to approva externally
+        INFT nftContractAddress = INFT(_contractAddress);
+        uint256 depositId = _depositorIdCounter.current();
+        _depositorIdCounter.increment();
+        depositors[depositId].nftOwner = msg.sender;
+        depositors[depositId].nftContract = _contractAddress;
+        depositors[depositId].tknIdOwner = tokenId;
+        depositors[depositId].depositId = depositId;
+        depositors[depositId].claimed = false;
+        nftContractAddress.safeTransferFrom(msg.sender, address(this), tokenId, "0x0");
+    }
+
+    function withdrawToken(INFT token, uint256 _tokenId, uint256 depositId, address _winner) public  {
+       require(depositors[depositId].claimed == false, "Already Claimed");
+       depositors[depositId].claimed = true;
+       token.safeTransferFrom(address(this), _winner, _tokenId, "0x0");
+    }
+
+    function onERC721Received(
+        address,
+        address from,
+        uint256 tokenId,
+        bytes memory
+    ) public override returns (bytes4) {
+       // require(paperNft.ownerOf(tokenId) == address(this), "MALICIOUS");
+       // require(paperNft.ownerOf(tokenId) == from, "user must be the owner of the token");
+
+        // depositor.nftOwner = from;
+        // depositor.tknIdOwner = tokenId;
+        ++_tokenIdCounter;
+        ++tokensRecieved;
+      
+        return this.onERC721Received.selector;
+    }  
+}
+
